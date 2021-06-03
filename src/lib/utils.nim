9f2f72892d9ac931807f24e03ea7ed8c4503787e
Florian Schroedl <flo.schroedl@gmail.com>
2021-06-03 13:41:04 +0200

    Add fp utils

--- src/lib/utils.nim
+++ src/lib/utils.nim
@@ -5,13 +5,6 @@ import fp/option
 import fp/either
 import sugar
 
-proc bitap*[T](xs: Option[T], errFn: () -> void, succFn: T -> void): Option[T] =
-  if (xs.isDefined):
-    succFn(xs.get)
-  else:
-    errFn()
-  xs
-
 proc sh*(cmd: string, workingDir = ""): Either[string, string] =
   let (res, exitCode) = execCmdEx(cmd, workingDir=workingDir)
   if exitCode == 0:
@@ -21,3 +14,22 @@ proc sh*(cmd: string, workingDir = ""): Either[string, string] =
   return res
     .strip
     .left(string)
+
+## Option
+
+proc bitap*[T](xs: Option[T], errFn: () -> void, succFn: T -> void): Option[T] =
+  if (xs.isDefined):
+    succFn(xs.get)
+  else:
+    errFn()
+  xs
+
+## Either
+
+proc tap*[E,A,B](e: Either[E,A], f: A -> B): Either[E,A] =
+  if e.isRight: discard f(e.get)
+  e
+
+proc log*[E,A](e: Either[E,A]): Either[E,A] =
+  echo $e
+  e
