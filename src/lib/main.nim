34196203ba62af53d1598efee8a9180618109abc
Florian Schroedl <flo.schroedl@gmail.com>
2021-06-03 17:43:27 +0200

    tmp

--- src/lib/main.nim
+++ src/lib/main.nim
@@ -1,4 +1,3 @@
-import types
 import utils
 import fp/option
 import fp/either
@@ -6,6 +5,8 @@ import sugar
 import strformat
 import tempfile
 import os
+
+const BACKUPDIR = "/tmp/org-print-scan-pdf-backup-dir"
 
 const SCAN_FILE_NAME = "out.pnm"
 const SCAN_CMD = &"""scanimage \
@@ -19,49 +20,86 @@ proc preparePassedFile(path: string, workingDir: string): Either[string, string]
     # Convert to tif format and remove the alpha channel
     # ocrmypdf doesn't work with alpha channels
     let inPath = absolutePath(path)
-    let outFile = changeFileExt(inPath, "tif")
+    let outFile = changeFileExt(path, "tif")
         .extractFilename
     let outPath = joinPath(workingDir, outFile)
 
     sh(&"convert {inPath} -alpha off {outPath}", workingDir)
         .map((x: string) => outPath)
 
+proc getOutput(path: Option[string], workingDir: string, isScan: bool): Option[string] =
+    let path = path
+        .map((x: string) => absolutePath(x))
+
+    if path.isEmpty and isScan:
+        let (_, name) = mkstemp()
+        let filename = name.Some
+            .map((x: string) => extractFilename(x))
+            .map((x: string) => changeFileExt(x, "pdf"))
+            .map((x: string) => joinPath(workingDir, x))
+
+        let fileExists = filename
+            .filter((x: string) => not fileExists(x))
+            .isEmpty
+
+        if fileExists:
+            raise newException(Exception,
+                    &"No output file passed and generated filename {$filename} exists")
+        else:
+            filename
+
+    else:
+        path
+
+proc saveFinal(input: string, output: string): Either[string, string] =
+
+
+    if fileExists(output):
+        let backupDir = mkdtemp(dir = BACKUPDIR)
+        let backupFile = joinPath(backupDir, extractFilename(output))
+        moveFile(output, backupFile)
+
+        sh("qpdf --empty --pages {backupFile} {input} -- {output}")
+            .map((x: string) => output)
+    else:
+        let (_, _, ext) = splitFile(output)
+
+        moveFile(input, output)
+        output.right("")
+
 proc main*(input = "", output = ""): any =
-    # let input = input.Some.notEmpty
-    # let output = output.Some.notEmpty
+    let isScan = input == ""
+    let workingDir = mkdtemp()
 
-    # echo typeof input
-    discard input.Some.notEmpty.map((x: string) => "foo")
+    let output = getOutput(
+      path = output.Some.notEmpty,
+      workingDir = workingDir,
+      isScan = isScan
+    )
 
-    # let workingDir = mkdtemp()
+    let ocrPdfPath = joinPath(workingDir, "out.pdf")
 
-    # let ocrPdfPath = joinPath(workingDir, "out.pdf")
+    # When no file name is passed execute scan & processing command
+    let filename = input
+        .Some
+        .notEmpty
+        .fold(
+          () => sh(SCAN_CMD, workingDir)
+            .flatMap((x: string) => sh(PROCESS_SCAN_CMD, workingDir))
+            .map(x => "out.tif")
+            .map(x => joinPath(workingDir, x)),
+          x => x
+            .rightS
+            .flatMap((path: string) => preparePassedFile(path, workingDir))
+        )
+        # OCR the input file
+        .flatMap((x: string) => sh(&"ocrmypdf {x} {ocrPdfPath} --image-dpi 72")
+            .map((x: string) => ocrPdfPath)
+        )
 
-    # # let output = opts.output.asEither("No file supplied")
-
-    # # let outPath = output
-    # #     .map((x: string) => changeFileExt(x, "pdf"))
-    # #     .orElse(() => opts.input
-    # #             .map((x: string) => changeFileExt(x, "pdf"))
-    # #             .filter((x: string) => fileExists(x))
-    # #             .asEither("File exists.")
-    # #     )
-
-    # # if (outPath.isLeft): raise newException(Exception, outPath.getLeft)
-
-    # # When no file name is passed execute scan & processing command
-    # let filename = input
-    #     .fold(
-    #       () => sh(SCAN_CMD, workingDir)
-    #         .flatMap((x: string) => sh(PROCESS_SCAN_CMD, workingDir))
-    #         .map(x => "out.tif")
-    #         .map(x => joinPath(workingDir, x)),
-    #       x => x
-    #         .rightS
-    #         .flatMap((path: string) => preparePassedFile(path, workingDir))
-    #     )
-    #     # OCR the input file
-    #     .flatMap((x: string) => sh(&"ocrmypdf {x} {ocrPdfPath} --image-dpi 72"))
-
-    # echo $filename
+    if filename.isRight:
+        echo filename.getOrElse("")
+        echo output.getOrElse("")
+        discard saveFinal(input = filename.getOrElse(""), output = output.getOrElse(""))
+        echo "Finish saving file"
     ""
