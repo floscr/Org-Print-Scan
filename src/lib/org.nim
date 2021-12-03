13f36b96b37a1f49257ebb3a474781eb89ad4551
Florian Schroedl <flo.schroedl@gmail.com>
2021-12-03 17:00:00 +0100

    Add headline property blocks

--- src/lib/org.nim
+++ src/lib/org.nim
@@ -9,6 +9,8 @@ import zero_functional
 
 const ORG_FILE_HEADER_TITLE_KEY = "TITLE"
 
+type orgProperty = (string, string)
+
 proc makeFileHeader*(key: string, value: string): string =
   ## Create an org file header with `key` and `value`.
   ## The key will be converted to uppercase per convention.
@@ -19,6 +21,19 @@ proc makeFileTitleHeader*(value: string): string =
   ## Create an org file title header
   ## E.g.: #+TITLE: `value`
   makeFileHeader(key = ORG_FILE_HEADER_TITLE_KEY, value)
+
+proc makeHeadlineProperties*(properties: seq[orgProperty] = @[]): string =
+  if properties.len == 0:
+    ""
+  else:
+    let propertiesStr = properties
+    .map((x: orgProperty) => &":{x[0].toUpper()}: {x[1]}")
+    .join(sep = "\n")
+
+    &""":PROPERTIES:
+{propertiesStr}
+:END:
+"""
 
 proc makeHeadlineTags*(tags: seq[string] = @[]): string =
   if tags.len == 0:
@@ -34,6 +49,7 @@ proc makeHeadline*(
   title: string,
   level = 1,
   tags: seq[string] = @[],
+  properties: seq[orgProperty] = @[],
 ): string =
   let stars = "*".repeat(level).just()
   let title = title.just()
@@ -42,9 +58,23 @@ proc makeHeadline*(
   .just()
   .notEmpty()
 
-  @[stars, title, tags] --> filter(it.isDefined())
+  let properties = properties
+  .makeHeadlineProperties()
+  .just()
+  .notEmpty()
+
+  let headline = @[
+    stars, title, tags,
+  ] --> filter(it.isDefined())
   .map(it.get())
   .reduce(it.accu & " " & it.elem)
+
+  @[
+    headline.just(),
+    properties,
+  ] --> filter(it.isDefined())
+  .map(it.get())
+  .reduce(it.accu & "\n" & it.elem)
 
 proc initFile*(
   fileTitleHeader = Nothing[string](),
@@ -66,11 +96,22 @@ proc initFile*(
 when isMainModule:
   import unittest
 
-  test "makeHeadline":
-    check: makeHeadline("Foo", tags = @["foo", "bar"]) == "* Foo :FOO:BAR:"
-    check: makeHeadline("Foo", level = 3) == "*** Foo"
+  test "Properties Block":
+    check: makeHeadlineProperties(@[("foo", "bar")]) == """:PROPERTIES:
+:FOO: bar
+:END:
+"""
 
-  test "initFile":
+  test "Headline":
+    check: makeHeadline("Foo", level = 3) == "*** Foo"
+    check: makeHeadline("Foo", tags = @["foo", "bar"]) == "* Foo :FOO:BAR:"
+    check: makeHeadline("Foo", properties = @[("foo", "bar")]) == """* Foo
+:PROPERTIES:
+:FOO: bar
+:END:
+"""
+
+  test "Empty File":
     check: initFile() == ""
     check: initFile(fileTitleHeader = "Foo".just()) == "#+TITLE: Foo"
     check: initFile(rootHeadline = "Foo".just()) == "* Foo"
